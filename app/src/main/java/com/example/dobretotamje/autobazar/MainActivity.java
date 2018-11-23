f2a673c01b2d34b65f17a7597cb821ee998c6667
Martin Krybus <m.krybus@email.cz>
2018-11-23 07:40:10 +0100

    orm done add adapter wip

--- app/src/main/java/com/example/dobretotamje/autobazar/MainActivity.java
+++ app/src/main/java/com/example/dobretotamje/autobazar/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.dobretotamje.autobazar;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.dobretotamje.autobazar.ORM.znacka;
@@ -22,43 +25,67 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LinkedList<znacka> select = znackaTable.Select();
-        CustomAdapterOptimized mAdapter = new CustomAdapterOptimized(this.getContext)
-
+        ZnackaAdapter mAdapter = new ZnackaAdapter(this.getApplicationContext(), R.layout.listview_znacka, select);
+        ListView lv = findViewById(R.id.lstviewZnacky);
+        lv.setAdapter(mAdapter);
     }
 
-    private class CustomAdapterOptimized extends ArrayAdapter<znacka> {
+    ListView.OnItemClickListener myListener = new ListView.OnItemClickListener(){
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            ZnackaAdapter.ZnackaHolder entry = (ZnackaAdapter.ZnackaHolder) adapterView.getItemAtPosition(i);
+            Intent selectAutoActivity = new Intent(getBaseContext(), SelectAutoActivity.class);
+            selectAutoActivity.putExtra("znackaId", entry.txtId.getText());
+            startActivity(selectAutoActivity);
+        }
+    };
 
-        public CustomAdapterOptimized(Context context, int textViewResourceId, List<znacka> objects) {
-            super(context, textViewResourceId, objects);
+    private class ZnackaAdapter extends ArrayAdapter<znacka> {
+
+        Context context;
+        int layoutResourceId;
+        List<znacka> data;
+
+        private ZnackaAdapter(Context context, int layoutResourceId, List<znacka> data) {
+            super(context, layoutResourceId, data);
+            this.layoutResourceId = layoutResourceId;
+            this.context = context;
+            this.data = data;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return getViewOptimize(position, convertView, parent);
-        }
+            View row = convertView;
+            ZnackaHolder znackaHolder;
+            if (row == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                row = inflater.inflate(layoutResourceId, parent, false);
 
-        public View getViewOptimize(int position, View convertView, ViewGroup parent) {
-            ViewHolder viewHolder;
-            if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater)
-                        getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(R.layout.example_item, null);
-                viewHolder = new ViewHolder();
-                viewHolder.exampleID = convertView.findViewById(R.id.exampleID);
-                viewHolder.exampleName = convertView.findViewById(R.id.exampleName);
-                convertView.setTag(viewHolder);
+                znackaHolder = new ZnackaHolder();
+                znackaHolder.txtNazev = row.findViewById(R.id.txtNazev);
+                znackaHolder.txtZemeVyroby = row.findViewById(R.id.txtZemeVyroby);
+                znackaHolder.textKoncern = row.findViewById(R.id.txtKoncern);
+                znackaHolder.txtId = row.findViewById(R.id.txtId);
+
+                row.setTag(znackaHolder);
             } else {
-                viewHolder = (ViewHolder) convertView.getTag();
+                znackaHolder = (ZnackaHolder) row.getTag();
             }
-            znacka example = getItem(position);
-            viewHolder.exampleID.setText(example.ID);
-            viewHolder.exampleName.setText(example.name);
-            return convertView;
+
+            znacka zna = data.get(position);
+            znackaHolder.txtNazev.setText(zna.Nazev);
+            znackaHolder.txtZemeVyroby.setText(zna.Zeme_vyroby);
+            znackaHolder.textKoncern.setText(zna.Koncern);
+            znackaHolder.textKoncern.setText(zna.Zn_id);
+
+            return row;
         }
 
-        private class ViewHolder {
-            public TextView exampleID;
-            public TextView exampleName;
+        private class ZnackaHolder {
+            TextView txtNazev;
+            TextView txtZemeVyroby;
+            TextView textKoncern;
+            TextView txtId;
         }
     }
 }
--- app/src/main/java/com/example/dobretotamje/autobazar/SelectAutoActivity.java
+++ app/src/main/java/com/example/dobretotamje/autobazar/SelectAutoActivity.java
@@ -1,7 +1,10 @@
 package com.example.dobretotamje.autobazar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.dobretotamje.autobazar.ORM.autoTable;
 
 public class SelectAutoActivity extends Activity {
 
@@ -9,5 +12,11 @@ public class SelectAutoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_auto);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if(bundle != null){
+            String znId = (String)bundle.get("znackaId");
+            autoTable.Select_Zn_Id(Integer.parseInt(znId));
+        }
     }
 }
